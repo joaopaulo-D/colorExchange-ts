0689c95578c203b6a52a7f5cbb37068bf5cfbf83
Joao Paulo <joaopaulodeoliveira2003@gmail.com>
2021-04-29 19:19:27 -0300

    first commit

---
+++ src/styles/theme/dark.ts
@@ -0,0 +1,11 @@
+export default {
+    title: 'dark',
+  
+    colors: {
+      primary: '#191919',
+      secundary: '#C62E65',
+  
+      background: '#a52ec6',
+      text: '#2e8cc6'
+    },
+  };
---
+++ src/styles/theme/light.ts
@@ -0,0 +1,11 @@
+export default {
+    title: 'light',
+
+    colors: {
+        primary: '#191919',
+        secundary: '#d63af9',
+
+        background: '#2e8cc6',
+        text: '#2e8cc6'
+    },
+};
